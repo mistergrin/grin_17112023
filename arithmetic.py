fe1760b58a2e7dddcb925e6319aef7e5cc932919
“Mister_Grin” <vanogrishin2007@gmail.com>
2023-11-24 10:14:21 +0100

    deleted last lines

--- arithmetic.py
+++ arithmetic.py
@@ -9,3 +9,5 @@ all_money = (quantity_ages * average_salary * 12) / 37.3
 cars_quantity = all_money // 31500
 
 print(f"я, {name_user.strip().title()}, зможу заробити лише {all_money} доларів, що вистачить лише на {cars_quantity} тойот, мене це не влаштовує, тому я буду змінювати своє життя і буду завзято вивчати програмування! ")
+
+
