da07c6de631fef0bfd7193d98455f4a5c588da22
“Mister_Grin” <vanogrishin2007@gmail.com>
2023-11-24 10:18:37 +0100

    deleted 2 strings

--- grin_first_file.py
+++ grin_first_file.py
@@ -1,4 +1,3 @@
 city_information = " Vienna is the capital of Austria. This city founded in 996. The buildings are built in Art Nouveau style. "
 print(city_information)
-string1 = 'fsf5342bv'
-print(string1.startswith('f'))
+
