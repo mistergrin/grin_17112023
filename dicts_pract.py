41128201d9103e3da1e49dc82ca7b17995fa5887
“Mister_Grin” <vanogrishin2007@gmail.com>
2023-12-05 23:17:47 +0100

    hw_dicts_new

--- dicts_pract.py
+++ dicts_pract.py
@@ -6,8 +6,9 @@ response = requests.get(url=url_carts)
 data_from_net = response.json()
 carts = data_from_net["carts"]
 
-for items in carts:
-    products = items["products"]
-    for product in products:
-        if product['discountPercentage'] >= 15:
-            print(products["title"])
+for users_purchase in carts:
+    products = users_purchase["products"]
+    if users_purchase["userId"] == 56:
+        for product in products:
+            if product['discountPercentage'] >= 15:
+                print(product["title"])
