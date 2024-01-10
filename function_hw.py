4a2e10c58d494d5071059e260f7b48afcb126881
“Mister_Grin” <vanogrishin2007@gmail.com>
2024-01-10 19:50:10 +0100

    hw_function_file

--- function_hw.py
+++ function_hw.py
@@ -1,33 +1,27 @@
 from decimal import Decimal
 
 FROM_INCHES_TO_CENTIMETERS = 2.54
-centimetres = 12.7
+YEARS = 25
+PERCENTS = 0.35
+MONTH_IN_YEAR = 12
 
 
-def convert_centimeters_to_inches() -> Decimal:
+def convert_centimeters_to_inches(centimetres: int | float) -> Decimal:
     inches = centimetres / FROM_INCHES_TO_CENTIMETERS
     result = Decimal(str(inches)).quantize(Decimal('0.01'))
     return result
 
 
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
-def get_list_of_paired_numbers():
-    for numbers in list:
+def get_list_of_paired_numbers(list_numbers: list[int]) -> list[int]:
+    for numbers in list_numbers:
         if numbers % 2 != 0:
-            list.remove(numbers)
-    return list
+            list_numbers.pop(numbers)
+    return list_numbers
 
 
-YEARS = 25
-sum_payments = 120000000
-monthly_income = 55000
-
-
-def is_bank_issue_loan() -> bool:
-    monthly_payment = sum_payments / (YEARS * 12)
-    percent_income = monthly_income * 0.35
+def is_bank_issue_loan(sum_payments: float | int, monthly_income: float | int) -> bool:
+    monthly_payment = sum_payments / (YEARS * MONTH_IN_YEAR)
+    percent_income = monthly_income * PERCENTS
     if percent_income > monthly_payment:
         answer = True
     else:
