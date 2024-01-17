574730d46122bede4fb1c8302f1bcf639029cd8c
“Mister_Grin” <vanogrishin2007@gmail.com>
2024-01-17 23:51:38 +0100

    hw_decor

--- hw_decorator.py
+++ hw_decorator.py
@@ -5,9 +5,10 @@ def decorator(func):
     def wrapper(*args):
         with open('txt.file.txt', mode='a', encoding='utf-8') as file:
             start_time_function = time.asctime()
-            string = func(*args)
+            result = func(*args)
             end_time_function = time.asctime()
-            file.write(f"function started at {start_time_function}, ended at {end_time_function}. Result - {string}\n")
+            file.write(f"function started at {start_time_function}, ended at {end_time_function}. Result - {result}\n")
+
     return wrapper
 
 
@@ -17,9 +18,9 @@ def get_string(string):
 
 
 @decorator
-def get_another_string(another_string):
-    return another_string
+def get_number(number):
+    return number * 3
 
 
 get_string("some_string")
-get_another_string("another_string")
+get_number(34849932)
